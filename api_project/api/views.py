660094c38c9da15c980e65fa7f14c9d6af2cf35a
RudoCoder <rudomakuwe@gmail.com>
2025-08-10 10:39:57 +0200

    Book list

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+# View for listing books only.
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# View for retrieving, updating, and deleting a specific book.
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
